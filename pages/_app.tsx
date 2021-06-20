8a06d6c3d09a5010199e1ba10dba05f78e453644
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2021-06-20 15:34:36 -0300

    feat(adicionado e configurado eslint): eslint adicionado e configurado

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import '../src/styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../src/styles/Home.module.css'
 
-export default function Home() {
+export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
